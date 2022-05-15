3d4099469967e3d67ed198055d18eaa99fcdbab7
addis yonas <addyonas@gmail.com>
2022-05-15 12:22:58 -0700

    for task 1

---
+++ print_unsigned.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_unsigned - manage unsigned int
+ * @ap: arg list
+ * Return: value number of printed chars
+ */
+
+int print_unsigned(va_list ap)
+{
+	unsigned int argument = va_arg(ap, unsigned int);
+
+	return (print_unsigned_number(argument));
+}
