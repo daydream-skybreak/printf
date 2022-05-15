1332350ecd9d3b679449b17e01f405ff06f91049
addis yonas <addyonas@gmail.com>
2022-05-15 10:38:19 -0700

    task 0

--- main.h
+++ main.h
@@ -1,9 +1,33 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+
+
+int _putchar(char c);
+int _puts(char *str, int ascii);
+char *convert_base(unsigned long nb, unsigned int base, int upper);
+char *_strdup(char *str);
+int convert_alpha_numeric(int nb, int upper);
+char *convert_base_pointer(unsigned long p);
+
+/**
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
+ */
+
+typedef struct flags_printf
+{
+	char *c;
+	int (*f)(va_list);
+} flags_p;
 #endif
---
+++ string_functions.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+
+/**
+ * print_char - print single char
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_char(va_list ap)
+{
+	char c = va_arg(ap, int);
+
+	if (c == '\0')
+	{
+		return (write(1, &c, 1));
+	}
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_str - print string
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_str(va_list ap)
+{
+	char *argument = va_arg(ap, char *);
+	int sum = 0;
+
+	if (!argument)
+	{
+		sum += _puts("(null)", 0);
+		return (sum);
+	}
+
+	return (_puts(argument, 0));
+}
+
+/**
+ * print_percent -  print percent character
+ * @args: char
+ * Return: percent character
+ */
+
+int print_percent(va_list args __attribute__((unused)))
+{
+	return (_putchar('%'));
+}
+
